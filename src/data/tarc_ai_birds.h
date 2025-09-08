87cf8e56bcc14a8da8536ebf5bce28547dcaa79d
Hedara <hedara90@gmail.com>
2025-09-08 12:00:38 +0200

    Boss progress

--- src/data/tarc_ai_birds.h
+++ src/data/tarc_ai_birds.h
@@ -1,12 +1,38 @@
 const struct BossMoveList sArticuno1 =
 {
-    .numMoves = 4,
+    .numMoves = 5,
     .moves =
     {
-        MOVE_TACKLE,
+        MOVE_SNOWSCAPE,
+        MOVE_BLIZZARD,
+        MOVE_AIR_CUTTER,
+        MOVE_DUAL_WINGBEAT,
+        MOVE_FREEZE_DRY,
+    },
+};
+
+const struct BossMoveList sArticuno2 =
+{
+    .numMoves = 5,
+    .moves =
+    {
         MOVE_ICE_BEAM,
-        MOVE_HAZE,
-        MOVE_ROOST
+        MOVE_ANCIENT_POWER,
+        MOVE_AIR_CUTTER,
+        MOVE_DUAL_WINGBEAT,
+        MOVE_FREEZE_DRY,
+    },
+};
+const struct BossMoveList sArticuno3 =
+{
+    .numMoves = 5,
+    .moves =
+    {
+        MOVE_SNOWSCAPE,
+        MOVE_BLIZZARD,
+        MOVE_AIR_CUTTER,
+        MOVE_DUAL_WINGBEAT,
+        MOVE_FREEZE_DRY,
     },
 };
 
@@ -15,20 +41,68 @@ const struct BossMoveList sZapdos1 =
     .numMoves = 4,
     .moves =
     {
-        MOVE_TACKLE,
-        MOVE_ICE_BEAM,
-        MOVE_HAZE,
-        MOVE_ROOST
+        MOVE_THUNDERBOLT,
+        MOVE_ACROBATICS,
+        MOVE_ROOST,
+        MOVE_DISCHARGE
+    },
+};
+const struct BossMoveList sZapdos2 =
+{
+    .numMoves = 4,
+    .moves =
+    {
+        MOVE_THUNDER,
+        MOVE_ANCIENT_POWER,
+        MOVE_DUAL_WINGBEAT,
+        MOVE_ELECTRO_BALL,
+    },
+};
+const struct BossMoveList sZapdos3 =
+{
+    .numMoves = 5,
+    .moves =
+    {
+        MOVE_ELECTRIC_TERRAIN,
+        MOVE_THUNDERBOLT,
+        MOVE_THUNDERBOLT,
+        MOVE_ROOST,
+        MOVE_THUNDERBOLT
     },
 };
 
 const struct BossMoveList sMoltres1 =
 {
-    .numMoves = 4,
+    .numMoves = 5,
     .moves =
     {
-        MOVE_EMBER,
+        MOVE_SUNNY_DAY,
+        MOVE_FLAMETHROWER,
         MOVE_FIRE_SPIN,
+        MOVE_ROOST,
+        MOVE_BRAVE_BIRD,
+    },
+};
+
+const struct BossMoveList sMoltres2 =
+{
+    .numMoves = 3,
+    .moves =
+    {
+        MOVE_BRAVE_BIRD,
+        MOVE_FLARE_BLITZ,
+        MOVE_ROOST,
+    },
+};
+
+const struct BossMoveList sMoltres3 =
+{
+    .numMoves = 5,
+    .moves =
+    {
+        MOVE_FIRE_SPIN,
+        MOVE_SUNNY_DAY,
+        MOVE_OVERHEAT,
         MOVE_FIRE_BLAST,
         MOVE_ROOST
     },
@@ -39,9 +113,8 @@ const struct BossAi sArticunoAi =
     .phaseMoves =
     {
         &sArticuno1,
-        &sArticuno1,
-        &sArticuno1,
-        &sArticuno1,
+        &sArticuno2,
+        &sArticuno3,
     },
 };
 
@@ -50,9 +123,8 @@ const struct BossAi sZapdosAi =
     .phaseMoves =
     {
         &sZapdos1,
-        &sZapdos1,
-        &sZapdos1,
-        &sZapdos1,
+        &sZapdos2,
+        &sZapdos3,
     },
 };
 
@@ -61,8 +133,7 @@ const struct BossAi sMoltresAi =
     .phaseMoves =
     {
         &sMoltres1,
-        &sMoltres1,
-        &sMoltres1,
-        &sMoltres1,
+        &sMoltres2,
+        &sMoltres3,
     },
 };
--- src/data/tarc_ai_golems.h
+++ src/data/tarc_ai_golems.h
@@ -1,48 +1,149 @@
 const struct BossMoveList sRegirock1 =
 {
-    .numMoves = 4,
+    .numMoves = 5,
     .moves =
     {
-        MOVE_TACKLE,
-        MOVE_ICE_BEAM,
-        MOVE_HAZE,
-        MOVE_ROOST
+        MOVE_CURSE,
+        MOVE_ANCIENT_POWER,
+        MOVE_ROCK_SLIDE,
+        MOVE_STONE_EDGE,
+        MOVE_HEAD_SMASH
+    },
+};
+
+const struct BossMoveList sRegirock2 =
+{
+    .numMoves = 5,
+    .moves =
+    {
+        MOVE_CURSE,
+        MOVE_HAMMER_ARM,
+        MOVE_DRAIN_PUNCH,
+        MOVE_BULLDOZE,
+        MOVE_HEAD_SMASH
+    },
+};
+
+const struct BossMoveList sRegirock3 =
+{
+    .numMoves = 5,
+    .moves =
+    {
+        MOVE_ROCK_SLIDE,
+        MOVE_DOUBLE_EDGE,
+        MOVE_ACCELEROCK,
+        MOVE_BODY_PRESS,
+        MOVE_HEAD_SMASH
     },
 };
 
 const struct BossMoveList sRegice1 =
 {
+    .numMoves = 5,
+    .moves =
+    {
+        MOVE_FROSTWISP,
+        MOVE_ICE_BEAM,
+        MOVE_FLASH_CANNON,
+        MOVE_SNOWSCAPE,
+        MOVE_BLIZZARD,
+    },
+};
+
+const struct BossMoveList sRegice2 =
+{
     .numMoves = 4,
     .moves =
     {
-        MOVE_TACKLE,
+        MOVE_FREEZE_DRY,
+        MOVE_ANCIENT_POWER,
+        MOVE_CHARGE_BEAM,
+        MOVE_CHILLING_WATER,
+    },
+};
+
+const struct BossMoveList sRegice3 =
+{
+    .numMoves = 5,
+    .moves =
+    {
+        MOVE_SNOWSCAPE,
+        MOVE_BLIZZARD,
         MOVE_ICE_BEAM,
-        MOVE_HAZE,
-        MOVE_ROOST
+        MOVE_FREEZE_DRY,
+        MOVE_CHILLING_WATER,
     },
 };
 
 const struct BossMoveList sRegisteel1 =
 {
+    .numMoves = 5,
+    .moves =
+    {
+        MOVE_IRON_DEFENSE,
+        MOVE_AMNESIA,
+        MOVE_HARD_PRESS,
+        MOVE_HEAVY_SLAM,
+        MOVE_FLASH_CANNON,
+    },
+};
+
+const struct BossMoveList sRegisteel2 =
+{
     .numMoves = 4,
     .moves =
     {
-        MOVE_EMBER,
-        MOVE_FIRE_SPIN,
-        MOVE_FIRE_BLAST,
-        MOVE_ROOST
+        MOVE_IRON_HEAD,
+        MOVE_EARTHQUAKE,
+        MOVE_FLASH_CANNON,
+        MOVE_BODY_PRESS,
+    },
+};
+
+const struct BossMoveList sRegisteel3 =
+{
+    .numMoves = 3,
+    .moves =
+    {
+        MOVE_IRON_HEAD,
+        MOVE_FLASH_CANNON,
+        MOVE_BODY_PRESS,
     },
 };
 
 const struct BossMoveList sRegieleki1 =
 {
-    .numMoves = 4,
+    .numMoves = 5,
     .moves =
     {
-        MOVE_EMBER,
-        MOVE_FIRE_SPIN,
-        MOVE_FIRE_BLAST,
-        MOVE_ROOST
+        MOVE_THUNDER_CAGE,
+        MOVE_ELECTRIFY,
+        MOVE_ELECTROWEB,
+        MOVE_CHARGE_BEAM,
+        MOVE_ELECTROWEB
+    },
+};
+
+const struct BossMoveList sRegieleki2 =
+{
+    .numMoves = 2,
+    .moves =
+    {
+        MOVE_ELECTROWEB,
+        MOVE_ELECTRO_BALL,
+    },
+};
+
+const struct BossMoveList sRegieleki3 =
+{
+    .numMoves = 5,
+    .moves =
+    {
+        MOVE_THUNDER_CAGE,
+        MOVE_ELECTRIFY,
+        MOVE_ELECTROWEB,
+        MOVE_THUNDERBOLT,
+        MOVE_ZAP_CANNON
     },
 };
 
@@ -51,10 +152,34 @@ const struct BossMoveList sRegidrago1 =
     .numMoves = 4,
     .moves =
     {
-        MOVE_EMBER,
-        MOVE_FIRE_SPIN,
-        MOVE_FIRE_BLAST,
-        MOVE_ROOST
+        MOVE_SUBSTITUTE,
+        MOVE_DRAGON_ENERGY,
+        MOVE_DRAGON_CLAW,
+        MOVE_SCALE_SHOT,
+    },
+};
+
+const struct BossMoveList sRegidrago2 =
+{
+    .numMoves = 4,
+    .moves =
+    {
+        MOVE_FIRE_FANG,
+        MOVE_THUNDER_FANG,
+        MOVE_ICE_FANG,
+        MOVE_CRUNCH,
+    },
+};
+
+const struct BossMoveList sRegidrago3 =
+{
+    .numMoves = 4,
+    .moves =
+    {
+        MOVE_SUBSTITUTE,
+        MOVE_DRAGON_ENERGY,
+        MOVE_EARTH_POWER,
+        MOVE_DRACO_METEOR
     },
 };
 
@@ -63,9 +188,8 @@ const struct BossAi sRegirockAi =
     .phaseMoves =
     {
         &sRegirock1,
-        &sRegirock1,
-        &sRegirock1,
-        &sRegirock1,
+        &sRegirock2,
+        &sRegirock3,
     },
 };
 
@@ -74,9 +198,8 @@ const struct BossAi sRegiceAi =
     .phaseMoves =
     {
         &sRegice1,
-        &sRegice1,
-        &sRegice1,
-        &sRegice1,
+        &sRegice2,
+        &sRegice3,
     },
 };
 
@@ -85,9 +208,8 @@ const struct BossAi sRegisteelAi =
     .phaseMoves =
     {
         &sRegisteel1,
-        &sRegisteel1,
-        &sRegisteel1,
-        &sRegisteel1,
+        &sRegisteel2,
+        &sRegisteel3,
     },
 };
 
@@ -96,9 +218,8 @@ const struct BossAi sRegielekiAi =
     .phaseMoves =
     {
         &sRegieleki1,
-        &sRegieleki1,
-        &sRegieleki1,
-        &sRegieleki1,
+        &sRegieleki2,
+        &sRegieleki3,
     },
 };
 
@@ -107,8 +228,7 @@ const struct BossAi sRegidragoAi =
     .phaseMoves =
     {
         &sRegidrago1,
-        &sRegidrago1,
-        &sRegidrago1,
-        &sRegidrago1,
+        &sRegidrago2,
+        &sRegidrago3,
     },
 };
