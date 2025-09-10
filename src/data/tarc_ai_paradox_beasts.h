c7f0558673e023f421a63ec5d289799cb3c9683d
Hedara <hedara90@gmail.com>
2025-09-10 11:30:19 +0200

    Add paradox beasts moves

--- src/data/tarc_ai_paradox_beasts.h
+++ src/data/tarc_ai_paradox_beasts.h
@@ -3,10 +3,34 @@ const struct BossMoveList sWalkingWake1 =
     .numMoves = 4,
     .moves =
     {
-        MOVE_TACKLE,
-        MOVE_ICE_BEAM,
-        MOVE_HAZE,
-        MOVE_ROOST
+        MOVE_HYDRO_STEAM,
+        MOVE_DRAGON_PULSE,
+        MOVE_SCALD,
+        MOVE_FLAMETHROWER,
+    },
+};
+
+const struct BossMoveList sWalkingWake2 =
+{
+    .numMoves = 4,
+    .moves =
+    {
+        MOVE_CHILLING_WATER,
+        MOVE_HYDRO_STEAM,
+        MOVE_BRINE,
+        MOVE_DRACO_METEOR
+    },
+};
+
+const struct BossMoveList sWalkingWake3 =
+{
+    .numMoves = 4,
+    .moves =
+    {
+        MOVE_HYDRO_PUMP,
+        MOVE_DRAGON_PULSE,
+        MOVE_FLAMETHROWER,
+        MOVE_HYDRO_STEAM
     },
 };
 
@@ -15,10 +39,31 @@ const struct BossMoveList sGougingFire1 =
     .numMoves = 4,
     .moves =
     {
-        MOVE_TACKLE,
-        MOVE_ICE_BEAM,
-        MOVE_HAZE,
-        MOVE_ROOST
+        MOVE_NOBLE_ROAR,
+        MOVE_FLAME_CHARGE,
+        MOVE_HONE_CLAWS,
+        MOVE_FLAME_CHARGE,
+    },
+};
+
+const struct BossMoveList sGougingFire2 =
+{
+    .numMoves = 3,
+    .moves =
+    {
+        MOVE_STONE_EDGE,
+        MOVE_FLARE_BLITZ,
+        MOVE_DRAGON_CLAW,
+    },
+};
+
+const struct BossMoveList sGougingFire3 =
+{
+    .numMoves = 2,
+    .moves =
+    {
+        MOVE_FLARE_BLITZ,
+        MOVE_DUAL_CHOP,
     },
 };
 
@@ -27,10 +72,35 @@ const struct BossMoveList sRagingBolt1 =
     .numMoves = 4,
     .moves =
     {
-        MOVE_EMBER,
-        MOVE_FIRE_SPIN,
-        MOVE_FIRE_BLAST,
-        MOVE_ROOST
+        MOVE_CHARGE_BEAM,
+        MOVE_CHARGE_BEAM,
+        MOVE_THUNDERBOLT,
+        MOVE_DRAGON_PULSE,
+    },
+};
+
+const struct BossMoveList sRagingBolt2 =
+{
+    .numMoves = 3,
+    .moves =
+    {
+        MOVE_THUNDER,
+        MOVE_HURRICANE,
+        MOVE_THUNDERCLAP,
+    },
+};
+
+const struct BossMoveList sRagingBolt3 =
+{
+    .numMoves = 6,
+    .moves =
+    {
+        MOVE_DISCHARGE,
+        MOVE_ZAP_CANNON,
+        MOVE_DRACO_METEOR,
+        MOVE_HYPER_VOICE,
+        MOVE_THUNDER,
+        MOVE_HURRICANE,
     },
 };
 
@@ -39,9 +109,8 @@ const struct BossAi sWalkingWakeAi =
     .phaseMoves =
     {
         &sWalkingWake1,
-        &sWalkingWake1,
-        &sWalkingWake1,
-        &sWalkingWake1,
+        &sWalkingWake2,
+        &sWalkingWake3,
     },
 };
 
@@ -50,9 +119,8 @@ const struct BossAi sGougingFireAi =
     .phaseMoves =
     {
         &sGougingFire1,
-        &sGougingFire1,
-        &sGougingFire1,
-        &sGougingFire1,
+        &sGougingFire2,
+        &sGougingFire3,
     },
 };
 
@@ -61,8 +129,7 @@ const struct BossAi sRagingBoltAi =
     .phaseMoves =
     {
         &sRagingBolt1,
-        &sRagingBolt1,
-        &sRagingBolt1,
-        &sRagingBolt1,
+        &sRagingBolt2,
+        &sRagingBolt3,
     },
 };
