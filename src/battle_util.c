007336e11944bbddd1ee782476239ffb4ae71b35
Hedara <hedara90@gmail.com>
2025-09-21 21:04:46 +0200

    Fix Tango triggering when unable to move

--- src/battle_util.c
+++ src/battle_util.c
@@ -5578,7 +5578,8 @@ u32 AbilityBattleEffects(u32 caseID, u32 battler, u32 ability, u32 special, u32 
     case ABILITYEFFECT_MOVE_END_ATTACKER: // Same as above, but for attacker
         STORE_BATTLER_TRAITS(gBattlerAttacker);
 
-        if (SearchTraits(battlerTraits, ABILITY_TANGO) && gMovesInfo[gCurrentMove].danceMove)
+        if (SearchTraits(battlerTraits, ABILITY_TANGO) && gMovesInfo[gCurrentMove].danceMove
+         && !(gHitMarker & HITMARKER_UNABLE_TO_USE_MOVE))
             gBattleMons[gBattlerAttacker].danced = TRUE;
 
         if (SearchTraits(battlerTraits, ABILITY_POISON_TOUCH)
--- test/battle/ability/tango.c
+++ test/battle/ability/tango.c
@@ -18,3 +18,21 @@ SINGLE_BATTLE_TEST("Tango makes the attack after a dance move into a two-hit mov
         ANIMATION(ANIM_TYPE_MOVE, MOVE_CELEBRATE, opponent);
     }
 }
+
+SINGLE_BATTLE_TEST("Tango doesn't trigger if the mon was prevented from moving")
+{
+    GIVEN {
+        PLAYER(SPECIES_MARACTUS) { Ability(ABILITY_TANGO); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(opponent, MOVE_FAKE_OUT); MOVE(player, MOVE_RAIN_DANCE); }
+        TURN { MOVE(player, MOVE_SCRATCH); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_FAKE_OUT, opponent);
+        NOT ANIMATION(ANIM_TYPE_MOVE, MOVE_RAIN_DANCE, player);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_CELEBRATE, opponent);
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_SCRATCH, player);
+        HP_BAR(opponent);
+        NOT HP_BAR(opponent);
+    }
+}
