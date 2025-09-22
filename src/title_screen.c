ad5e03cfb1ebfa85c7374fec7f4cdeafae4c0f32
Hedara <hedara90@gmail.com>
2025-09-22 17:59:27 +0200

    Change title screen music

--- src/title_screen.c
+++ src/title_screen.c
@@ -670,7 +670,7 @@ void CB2_InitTitleScreen(void)
                                     | DISPCNT_OBJ_ON
                                     | DISPCNT_WIN0_ON
                                     | DISPCNT_OBJWIN_ON);
-        m4aSongNumStart(MUS_TITLE);
+        m4aSongNumStart(MUS_B_PYRAMID_TOP);
         gMain.state = 5;
         break;
     case 5:
