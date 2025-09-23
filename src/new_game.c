319f395f287f10562e69fcf8d52c4f26e7996687
Hedara <hedara90@gmail.com>
2025-09-23 20:47:56 +0200

    Display Trainer Id

--- src/new_game.c
+++ src/new_game.c
@@ -88,7 +88,7 @@ void CopyTrainerId(u8 *dst, u8 *src)
 
 static void InitPlayerTrainerId(void)
 {
-    u32 trainerId = (Random() << 16) | GetGeneratedTrainerIdLower();
+    u32 trainerId = Random32();
     SetTrainerId(trainerId, gSaveBlock2Ptr->playerTrainerId);
 }
 
