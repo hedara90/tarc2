c3214b0e5d91694209d7189802fdd49ab6c05db7
Turtleye15 <92760316+Turtleye15@users.noreply.github.com>
2025-09-24 00:18:13 +0200

    Kenichi

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -664,7 +664,7 @@
 #define FLAG_KENICHI_INTRO_CHAT  0x269 // Unused Flag
 #define FLAG_KENICHI_FALSE_ENDING_CHAT  0x26A // Unused Flag
 #define FLAG_KENICHI_TRUE_ENDING_CHAT  0x26B // Unused Flag
-#define FLAG_UNUSED_0x26C  0x26C // Unused Flag
+#define FLAG_KENICHI_FAMILY  0x26C // Unused Flag
 #define FLAG_UNUSED_0x26D  0x26D // Unused Flag
 #define FLAG_UNUSED_0x26E  0x26E // Unused Flag
 #define FLAG_UNUSED_0x26F  0x26F // Unused Flag
