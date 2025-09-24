543d46c8bf59abab3e78ea5119e96997e9ebe66d
Turtleye15 <92760316+Turtleye15@users.noreply.github.com>
2025-09-24 22:57:09 +0200

    GIratina script start nod

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -665,8 +665,8 @@
 #define FLAG_KENICHI_FALSE_ENDING_CHAT  0x26A // Unused Flag
 #define FLAG_KENICHI_TRUE_ENDING_CHAT  0x26B // Unused Flag
 #define FLAG_KENICHI_FAMILY  0x26C // Unused Flag
-#define FLAG_UNUSED_0x26D  0x26D // Unused Flag
-#define FLAG_UNUSED_0x26E  0x26E // Unused Flag
+#define FLAG_MONKS_HANZO  0x26D // Unused Flag
+#define FLAG_MONKS_NAKAMURA  0x26E // Unused Flag
 #define FLAG_UNUSED_0x26F  0x26F // Unused Flag
 #define FLAG_UNUSED_0x270  0x270 // Unused Flag
 #define FLAG_UNUSED_0x271  0x271 // Unused Flag
