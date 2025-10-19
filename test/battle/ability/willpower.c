07a59a7db12a9621fcf871dd0e8862de9aa7611c
Hedara <hedara90@gmail.com>
2025-10-19 10:11:45 +0200

    Fix Winds of Change BattleScript ending MoveEnd

--- test/battle/ability/willpower.c
+++ test/battle/ability/willpower.c
@@ -32,3 +32,20 @@ SINGLE_BATTLE_TEST("Willpower boosts Attack when using a special move")
         EXPECT_EQ(player->statStages[STAT_ATK], DEFAULT_STAT_STAGE + 1);
     }
 }
+
+SINGLE_BATTLE_TEST("Willpower doesn't prevent other effects from happening")
+{
+    GIVEN {
+        PLAYER(SPECIES_WOBBUFFET) { Ability(ABILITY_WILLPOWER); Item(ITEM_LIFE_ORB); }
+        OPPONENT(SPECIES_WOBBUFFET);
+    } WHEN {
+        TURN { MOVE(player, MOVE_HYPER_VOICE); }
+    } SCENE {
+        ANIMATION(ANIM_TYPE_MOVE, MOVE_HYPER_VOICE, player);
+        HP_BAR(opponent);
+        ANIMATION(ANIM_TYPE_GENERAL, B_ANIM_STATS_CHANGE, player);
+        HP_BAR(player);
+    } THEN {
+        EXPECT_EQ(player->statStages[STAT_ATK], DEFAULT_STAT_STAGE + 1);
+    }
+}
