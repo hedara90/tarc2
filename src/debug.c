b3632bb3f059bdbc477e72c564eb1aa8720b2146
Turtleye15 <92760316+Turtleye15@users.noreply.github.com>
2026-02-08 16:56:43 +0100

    Final intro script

--- src/debug.c
+++ src/debug.c
@@ -4915,7 +4915,9 @@ void AddStuff(void)
     FlagSet(FLAG_HELP_HEAL);
     gSaveBlock1Ptr->bestBosses[FINAL_BOSS_GIRATINA].teamMembers[0] = SPECIES_GALVANTULA;
     gSaveBlock1Ptr->bestBosses[FINAL_BOSS_XERNEAS].teamMembers[0] = SPECIES_GALVANTULA;
+    gSaveBlock1Ptr->bestBosses[FINAL_BOSS_LUGIA].teamMembers[0] = SPECIES_GALVANTULA;
     VarSet(VAR_GIRATINA_PROGRESS, 3);
+    VarSet(VAR_STORY, 1);
 }
 
 u32 RomXor();
