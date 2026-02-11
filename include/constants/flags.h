8581406ccd7c68760bff9434c91973da965c01dd
Turtleye15 <92760316+Turtleye15@users.noreply.github.com>
2026-02-11 14:28:43 +0100

    Start of post victory script

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -676,7 +676,7 @@
 #define FLAG_SASTUKI_THREEMYTHS  0x275 // Unused Flag
 #define FLAG_CHISA_CHAT 0x276 // Unused Flag
 #define FLAG_CHISA_ENTRANCE  0x277 // Unused Flag
-#define FLAG_UNUSED_0x278  0x278 // Unused Flag
+#define FLAG_SATSUKI_ENTRANCE  0x278 // Unused Flag
 #define FLAG_UNUSED_0x279  0x279 // Unused Flag
 #define FLAG_UNUSED_0x27A  0x27A // Unused Flag
 #define FLAG_UNUSED_0x27B  0x27B // Unused Flag
